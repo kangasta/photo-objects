830983d5957f2575bb8d7d808ec1226078dc9d9d
Toni Kangas <toni.ar.kangas@gmail.com>
2024-08-14 22:55:45 +0300

    Configure urls for Django UI

--- api/photo_objects/urls.py
+++ api/photo_objects/urls.py
@@ -5,10 +5,11 @@ from . import views
 app_name = "photos"
 urlpatterns = [
     path("_auth", views.has_permission),
-    path("albums", views.albums),
-    path("albums/<str:album_key>", views.album),
-    path("albums/<str:album_key>/photos", views.photos),
-    path("albums/<str:album_key>/photos/<str:photo_key>", views.photo),
-    path("albums/<str:album_key>/photos/<str:photo_key>/img", views.get_img),
+    path("api/albums", views.albums),
+    path("api/albums/<str:album_key>", views.album),
+    path("api/albums/<str:album_key>/photos", views.photos),
+    path("api/albums/<str:album_key>/photos/<str:photo_key>", views.photo),
+    path("api/albums/<str:album_key>/photos/<str:photo_key>/img", views.get_img),
     # TODO: img/<str:album_key>/<str:photo_key>/<str:size_key> path
+    # TODO: ui views
 ]
