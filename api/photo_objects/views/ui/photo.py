bc61ec881773ad23d2242d0aaf2d4af757047b49
Toni Kangas <toni.ar.kangas@gmail.com>
2024-08-16 03:05:38 +0300

    Add show and delete photo views

--- api/photo_objects/views/ui/photo.py
+++ api/photo_objects/views/ui/photo.py
@@ -6,14 +6,23 @@ from photo_objects import api
 from photo_objects.api.utils import FormValidationFailed
 from photo_objects.forms import ModifyPhotoForm
 
+from .utils import json_problem_as_html
+
+
+@json_problem_as_html
 def upload_photo(request: HttpRequest, album_key: str):
     return HttpResponse("upload_photo")
 
 
+@json_problem_as_html
 def show_photo(request: HttpRequest, album_key: str, photo_key: str):
-    return HttpResponse("show_photo")
+    photo = api.check_photo_access(request, album_key, photo_key, "lg")
+
+    return render(request, "photo_objects/photo/show.html",
+                  {"photo": photo})
 
 
+@json_problem_as_html
 def edit_photo(request: HttpRequest, album_key: str, photo_key: str):
     if request.method == "POST":
         try:
@@ -34,5 +43,11 @@ def edit_photo(request: HttpRequest, album_key: str, photo_key: str):
     return render(request, 'photo_objects/form.html', {"form": form, "title": "Edit photo"})
 
 
+@json_problem_as_html
 def delete_photo(request: HttpRequest, album_key: str, photo_key: str):
-    return HttpResponse("delete_photo")
+    if request.method == "POST":
+        api.delete_photo(request, album_key, photo_key)
+        return HttpResponseRedirect(reverse('photo_objects:show_album', kwargs={"album_key": album_key}))
+    else:
+        album = api.check_album_access(request, album_key)
+    return render(request, 'photo_objects/photo/delete.html', {"album": album})
