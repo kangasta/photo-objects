dde5c3f03bd131dd5f26830cbc4d2a8d0aec3ffe
Toni Kangas <toni.ar.kangas@gmail.com>
2024-11-24 00:47:24 +0200

    Move photo_objects module to repository root

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.10
+
+WORKDIR /app
+
+COPY manifest.in pyproject.toml requirements.txt ./
+RUN pip install -r requirements.txt
+
+COPY back/api/ ./api/
+COPY photo_objects/ ./photo_objects/
+RUN pip install .
+COPY back/entrypoint.sh back/manage.py ./
+ENTRYPOINT ["./entrypoint.sh"]
---
+++ runtests.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+import sys
+
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+
+if __name__ == "__main__":
+    sys.path.append(os.path.join(os.path.dirname(__file__), "back"))
+    os.environ["DJANGO_SETTINGS_MODULE"] = "api.settings"
+    django.setup()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
+    failures = test_runner.run_tests(["photo_objects.django.tests"])
+    sys.exit(min(failures, 250))
