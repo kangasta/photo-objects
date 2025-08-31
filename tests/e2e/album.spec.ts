28c5ff05e9ced41f2bd8fb18b4d7867d09474ef6
Toni Kangas <toni.ar.kangas@gmail.com>
2025-08-31 23:56:01 +0300

    Validate success and error messages

--- tests/e2e/album.spec.ts
+++ tests/e2e/album.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@playwright/test';
-import { createAlbum, deleteAlbum, login, uploadPhotos } from './actions';
+import { checkTitlesExist, createAlbum, deleteAlbum, login, uploadPhotos } from './actions';
 
 let albumTitle: string;
 
@@ -11,6 +11,11 @@ test('create album and upload photo', async ({ page }) => {
   await expect(page.getByText('Private')).toBeVisible();
 
   await uploadPhotos(page, albumTitle, ['bus-stop.jpg', "tower.jpg"]);
+  await checkTitlesExist(page, ['bus-stop.jpg', "tower.jpg"]);
+
+  await uploadPhotos(page, albumTitle, ["tower.jpg", "havfrue.jpg"]);
+  await expect(page.getByText('Successfully uploaded 1 photo.')).toBeVisible();
+  await expect(page.getByText('Failed to upload tower.jpg. Photo with this filename already exists in the album.')).toBeVisible();
 });
 
 test.afterEach(async ({ page }) => {
